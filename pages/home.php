b32632f1e915f1d8756fe51cff112be8cbc17085
Leonardo Ribeiro <leorsousa05@gmail.com>
2024-06-17 15:32:27 -0300

    fix: Fix System Imports

--- pages/home.php
+++ pages/home.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <?php include 'includes/configurations/head.php'; ?>
+
 <body>
-    <?php include 'includes/components/header.php' ?> 
-    <main>
-        Home
-    </main>
-    <?php include 'includes/components/footer.php' ?>
-    <?php include 'includes/configurations/schema_data.php' ?>
+	<?php include 'includes/components/header.php' ?>
+	<main>
+		Home
+	</main>
+	<?php include 'includes/components/footer.php' ?>
+	<?php include 'includes/configurations/schemaData.php' ?>
 </body>
+
 </html>
