b2587fc2aff4639aa437eaab2810cc29ea3768c8
Leonardo Ribeiro <leorsousa05@gmail.com>
2024-12-06 16:14:19 -0300

    feat: new schema template

--- src/utils/schemaData.php
+++ src/utils/schemaData.php
@@ -10,60 +10,41 @@ $language = "pt-BR";
 $stars = '4.' . rand(1, 9);
 $comments = rand(35, 52);
 
-?>
+$schema = [
+	"@context" => "https://schema.org",
+	"@type" => "Organization",
+	"name" => $websiteName ?: "Your Company Name", // Default to placeholder if empty
+	"url" => "https://" . $websiteUrl,
+	"description" => $websiteDescription ?: "Your company description",
+	"keywords" => $websiteKeywords ?: "Your keywords",
+	"author" => $websiteAuthor ?: "Your Company Author",
+	"logo" => $logoLocation,
+	"address" => [
+		"@type" => "PostalAddress",
+		"streetAddress" => $address,
+		"addressLocality" => $city,
+		"addressRegion" => $state,
+		"postalCode" => $cep,
+		"addressCountry" => $country
+	],
+	"geo" => [
+		"@type" => "GeoCoordinates",
+		"latitude" => explode(",", $geoPosition)[0] ?? null,
+		"longitude" => explode(",", $geoPosition)[1] ?? null
+	],
+	"telephone" => $phoneNumber,
+	"aggregateRating" => [
+		"@type" => "AggregateRating",
+		"ratingValue" => $stars,
+		"reviewCount" => $comments
+	],
+	"sameAs" => [
+		"https://facebook.com/yourcompany", // Add social media links
+		"https://twitter.com/yourcompany",
+		"https://linkedin.com/company/yourcompany"
+	],
+	"areaServed" => $geoRegion,
+	"isicV4" => $classification ?: "Your ISIC classification"
+];
 
-
-<script type="application/ld+json">
-	{
-		"@context": "https://schema.org",
-		"@type": "Organization",
-		"mainEntityOfPage": "<?= $websiteUrl ?>",
-		"name": "<?= $websiteName ?>",
-		"url": "<?= $websiteUrl ?>",
-		"logo": "<?= $websiteUrl . IMAGE_FOLDER .  "logo.webp" ?>",
-		"description": "<?= $websiteDescription ?>",
-		"aggregateRating": {
-			"@type": "AggregateRating",
-			"ratingValue": "<?= $stars ?>",
-			"reviewCount": "<?= $comments ?>"
-		},
-		"contactPoint": {
-			"@type": "ContactPoint",
-			"telephone": "<?= $phoneNumber ?>",
-			"contactType": "Customer service"
-		},
-		"sameAs": [
-			"https://www.facebook.com/DigitallEvolutionMarketing/",
-			"https://www.linkedin.com/company/digitall-evolution/",
-			"https://www.youtube.com/channel/UCINuoDTdzAZJczr4MocH8SA",
-			"https://www.instagram.com/digitallevolutionmarketing/"
-		],
-		"address": {
-			"@type": "PostalAddress",
-			"streetAddress": "<?= $address ?>",
-			"addressLocality": "<?= $city . "/" . $state ?>",
-			"addressRegion": "$city",
-			"postalCode": "CEP: <?= $cep ?>",
-			"addressCountry": "<?= $country ?>"
-		}
-	}, {
-		"thumbnailUrl": "<?= $websiteUrl . IMAGE_FOLDER . "logo-data.webp" ?>",
-		"description": "<?= $websiteDescription ?>",
-		"inLanguage": "<?= $language ?>",
-		"potentialAction": [{
-			"@type": "ReadAction",
-			"target": [
-				"<?= $websiteUrl ?>"
-			]
-		}]
-	}, {
-		"@type": "ImageObject",
-		"inLanguage": "<?= $language ?>",
-		"@id": "<?= $websiteUrl ?>",
-		"url": "<?= $websiteUrl . IMAGE_FOLDER . "logo-data.webp" ?>",
-		"contentUrl": "<?= $websiteUrl . IMAGE_FOLDER . "logo-data.webp" ?>",
-		"width": 768,
-		"height": 768,
-		"caption": "Logo <?= $websiteName ?>"
-	}
-</script>
+echo '<script type="application/ld+json">' . json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . '</script>';
